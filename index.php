e50a9964fa81b400d6270dcebd45078c2551d60b
MD15 <36522826+MD15@users.noreply.github.com>
2020-03-29 09:44:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <meta charset="utf-8">
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Beranda | info-corona.id</title>
+    <title>Rumah Sakit Rujukan | info-corona.id</title>
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="style.css">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css" rel="stylesheet"/>
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="../css/simple-sidebar.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="../style.css">
 
     <!-- Font Awesome JS -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
@@ -20,11 +23,12 @@
 </head>
 
 <body>
-<?php
+  <?php
 $url='https://api.kawalcorona.com/indonesia';
 $reqs = file_get_contents($url);
 $json = json_decode($reqs);
 ?>
+  <div class="d-flex" id="wrapper">
     <div class="wrapper">
         <!-- Sidebar  -->
         <nav id="sidebar">
@@ -33,7 +37,7 @@ $json = json_decode($reqs);
             </div>
 
             <ul class="list-unstyled components">
-                <p align="center">Live Corona Data</p>
+                <p align="center">Rumah Sakit Rujukan</p>
                 <li>
                   <a href="/">Beranda</a>
                 </li>
@@ -59,7 +63,8 @@ $json = json_decode($reqs);
         </nav>
 
         <!-- Page Content  -->
-        <div id="content">
+
+<div id="content">
 
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
@@ -109,11 +114,9 @@ $json = json_decode($reqs);
                 </div>
                 </div>
             <iframe src="./indo.html" height="500" width="100%" frameborder="0" class="pt-4"></iframe>
-            </div>
-            </div>
             <div class="container">
-            <label for="input">Cari provinsi</label>
-            <input type='text' id='input' onkeyup='searchTable()' placeholder="Cari berdasarkan provinsi" class="form-control" width="10px">
+            <label for="input" class="pt-2">Cari rumah sakit</label>
+            <input type='text' id='input' onkeyup='searchTable()' placeholder="Cari rumah sakit" class="form-control" width="10px">
             <br>
             <table class="table table-striped table-bordered" id="table" data-toggle="table">
               <thead>
@@ -153,18 +156,47 @@ $json = json_decode($reqs);
                 </tr>
               </tfoot>
             </table>
-            <h6 align="center">Data from <a href="https://inacovid19.maps.arcgis.com/apps/opsdashboard/index.html#/4411f5e9c69d4ca4be31ac805a0267be">https://inacovid19.maps.arcgis.com/ </a></h6>
-            </div>
-        </div>
-    </div>
-
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
+            <h6 align="center">Data from <a href="https://www.facebook.com/photo?fbid=2780181498702981"> Official Facebook Kominfo </h6>
+            </div><!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <!-- Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
+    <script>
+      function searchTable() {
+          var input;
+          var saring;
+          var status; 
+          var tbody; 
+          var tr; 
+          var td;
+          var i; 
+          var j;
+          input = document.getElementById("input");
+          saring = input.value.toUpperCase();
+          tbody = document.getElementsByTagName("tbody")[0];;
+          tr = tbody.getElementsByTagName("tr");
+          for (i = 0; i < tr.length; i++) {
+              td = tr[i].getElementsByTagName("td");
+              for (j = 0; j < td.length; j++) {
+                  if (td[j].innerHTML.toUpperCase().indexOf(saring) > -1) {
+                      status = true;
+                  }
+              }
+              if (status) {
+                  tr[i].style.display = "";
+                  status = false;
+              } else {
+                  tr[i].style.display = "none";
+              }
+          }
+      }
+    </script>
     <script type="text/javascript">
         $(document).ready(function () {
             $('#sidebarCollapse').on('click', function () {
